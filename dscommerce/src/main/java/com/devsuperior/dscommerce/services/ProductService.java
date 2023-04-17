26d436720437c0e9fc35c35ad48565362275a718
claudio <claudiomrbdev@gmail.com>
2023-04-17 14:28:31 -0300

    Implementando delete

--- dscommerce/src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -62,6 +62,13 @@ public class ProductService {
 		return new ProductDTO(entity);
 
 	}
+	
+	@Transactional
+	public void delete(Long id) {
+
+		repository.deleteById(id);
+		
+	}
 
 	private void copyDtoToEntity(ProductDTO dto, Product entity) {
 		entity.setName(dto.getName());
@@ -69,4 +76,5 @@ public class ProductService {
 		entity.setPrice(dto.getPrice());
 		entity.setImgUrl(dto.getImgUrl());
 	}
+	
 }
