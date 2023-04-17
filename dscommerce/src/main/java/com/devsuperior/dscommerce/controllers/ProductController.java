9f66de87f64522714e7a9db5fc174b2b9c878eb3
claudio <claudiomrbdev@gmail.com>
2023-04-17 10:42:05 -0300

    Implementando Put e jakarat bean validation

--- dscommerce/src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,8 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.dscommerce.dto.ProductDTO;
 import com.devsuperior.dscommerce.services.ProductService;
+
+import jakarta.validation.Valid;
 
 @RestController 
 @RequestMapping(value = "/products")
@@ -43,12 +46,22 @@ public class ProductController {
 	}
 	
 	@PostMapping
-	public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
+	public ResponseEntity<ProductDTO> insert(@Valid @RequestBody ProductDTO dto) {
 	
 		dto = service.insert(dto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
 		return ResponseEntity.created(uri).body(dto);		
 	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ProductDTO> findById(@PathVariable Long id, @Valid @RequestBody ProductDTO dto) {
+	
+		dto = service.update(id, dto);
+		
+	return ResponseEntity.ok(dto);
+		
+	}
+	
 	
 	//configuração do Endpoint para responder na rota "products" pelo método GET via API
 
